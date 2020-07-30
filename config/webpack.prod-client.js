707b72fdf67ff8cdd0b061225e3b55f762aedb06
Sebas <sebas.warsaw@gmail.com>
2020-07-30 13:19:04 -0300

    update webpack performance

--- config/webpack.prod-client.js
+++ config/webpack.prod-client.js
@@ -45,6 +45,9 @@ module.exports = {
         use: [
           {
             loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+            },
           },
         ],
       },
--- config/webpack.prod-server.js
+++ config/webpack.prod-server.js
@@ -32,6 +32,9 @@ module.exports = {
         use: [
           {
             loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+            },
           },
         ],
       },

1d68a21be15eed9ea1c8dc88a0b172461075df4f
Sebas <sebas.warsaw@gmail.com>
2020-08-02 12:02:46 -0300

    another one4

--- src/server/express.js
+++ src/server/express.js
@@ -13,6 +13,7 @@ import storeMiddleware from './middleware/store';
 import server from './auth-server/express';
 
 const expressStaticGzip = require('express-static-gzip');
+const cluster = require('cluster');
 
 // const server = require('./auth0/auth0');
 
@@ -21,55 +22,79 @@ const isDev = !isProd;
 const PORT = process.env.PORT || 8080;
 const HOST = process.env.SERVER_HOST || '0.0.0.0';
 let isBuilt = false;
+const numWorkers = process.env.WEB_CONCURRENCY || 1;
 
-const done = () => {
-  !isBuilt &&
-    server.listen(PORT, () => {
-      isBuilt = true;
-      console.log(
-        `Server listening on \x1b[42m\x1b[1mhttp://${HOST}:${PORT}\x1b[0m in \x1b[41m${process.env.NODE_ENV}\x1b[0m 🌎...`,
-      );
-    });
-};
+if (cluster.isMaster) {
+  // const numWorkers = require('os').cpus().length;
 
-if (isDev) {
-  const compiler = webpack([configDevClient, configDevServer]);
+  console.log(`Master cluster setting up ${numWorkers} workers...`);
 
-  const clientCompiler = compiler.compilers[0];
-  const serverCompiler = compiler.compilers[1]; // eslint-disable-line
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < numWorkers; i++) {
+    cluster.fork();
+  }
 
-  const webpackDevMiddleware = require('webpack-dev-middleware')(
-    compiler,
-    configDevClient.devServer,
-  );
-
-  const webpackHotMiddlware = require('webpack-hot-middleware')(
-    clientCompiler,
-    configDevClient.devServer,
-  );
-
-  server.use(storeMiddleware());
-  server.use(webpackDevMiddleware);
-  server.use(webpackHotMiddlware);
-  server.use(webpackHotServerMiddleware(compiler));
-  console.log('Middleware enabled');
-  done();
-} else {
-  webpack([configProdClient, configProdServer]).run((_err, stats) => {
-    const clientStats = stats.toJson().children[0];
-    const render = require('../../build/prod-server-bundle.js').default; // eslint-disable-line
-    console.log(
-      stats.toString({
-        colors: true,
-      }),
-    );
-    server.use(
-      expressStaticGzip('dist', {
-        enableBrotli: true,
-      }),
-    );
-    server.use(storeMiddleware());
-    server.use(render({ clientStats }));
-    done();
+  cluster.on('online', (worker) => {
+    console.log(`Worker ${worker.process.pid} is online`);
   });
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(
+      `Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`,
+    );
+    console.log('Starting a new worker');
+    cluster.fork();
+  });
+} else {
+  const done = () => {
+    !isBuilt &&
+      server.listen(PORT, () => {
+        isBuilt = true;
+        console.log(
+          `Server listening on \x1b[42m\x1b[1mhttp://${HOST}:${PORT}\x1b[0m in \x1b[41m${process.env.NODE_ENV}\x1b[0m 🌎...`,
+        );
+      });
+  };
+
+  if (isDev) {
+    const compiler = webpack([configDevClient, configDevServer]);
+
+    const clientCompiler = compiler.compilers[0];
+    const serverCompiler = compiler.compilers[1]; // eslint-disable-line
+
+    const webpackDevMiddleware = require('webpack-dev-middleware')(
+      compiler,
+      configDevClient.devServer,
+    );
+
+    const webpackHotMiddlware = require('webpack-hot-middleware')(
+      clientCompiler,
+      configDevClient.devServer,
+    );
+
+    server.use(storeMiddleware());
+    server.use(webpackDevMiddleware);
+    server.use(webpackHotMiddlware);
+    server.use(webpackHotServerMiddleware(compiler));
+    console.log('Middleware enabled');
+    done();
+  } else {
+    webpack([configProdClient, configProdServer]).run((_err, stats) => {
+      const clientStats = stats.toJson().children[0];
+      const render = require('../../build/prod-server-bundle.js').default; // eslint-disable-line
+      console.log(
+        stats.toString({
+          colors: true,
+        }),
+      );
+      server.use(
+        expressStaticGzip('dist', {
+          enableBrotli: true,
+        }),
+      );
+      server.use(storeMiddleware());
+      server.use(render({ clientStats }));
+      done();
+    });
+  }
 }
